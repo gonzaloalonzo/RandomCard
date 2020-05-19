45516a358571637e2d2342c3c09f91a8e72acc4c
gonzaloalonzo <gonzaloalonzo@hotmail.com>
2020-05-19 01:12:31 -0400

    card randon second step

---
+++ style.js
@@ -0,0 +1,22 @@
+
+let simbolos = ["♠", "♣","♥", "♦" ];
+let pintas = ["A", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K"];
+
+let i = Math.floor(Math.random()*simbolos.length);
+let cardsig = simbolos[i];
+
+
+
+let e = Math.floor(Math.random()*pintas.length);
+ let cardcenter = pintas[e];
+
+
+ 
+ (function cargar(){
+ 
+ document.querySelector(".card-pic1").innerHTML = cardsig;
+ document.querySelector(".card-center").innerHTML = cardcenter;
+ document.querySelector(".card-pic2").innerHTML = cardsig;
+ 
+})();
+
